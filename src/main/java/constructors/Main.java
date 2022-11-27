c9c8a0bbaf4300f686d2ecf590369bd4af266542
greeng00se <thegreengoose7@gmail.com>
2022-11-27 17:52:03 +0900

    생성자 탐색 및 객체 생성

---
+++ src/main/java/constructors/Main.java
@@ -0,0 +1,98 @@
+package constructors;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
+//        printConstructorData(Person.class);
+
+        Address address = createInstanceWithArguments(Address.class, "First", 10);
+        Person person = createInstanceWithArguments(Person.class, address, "John", 21);
+        System.out.println(person);
+    }
+
+    public static <T> T createInstanceWithArguments(Class<T> clazz, Object... args)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+            if (constructor.getParameterTypes().length == args.length) {
+                return (T) constructor.newInstance(args);
+            }
+        }
+        System.out.println("An appropriate constructor was not found");
+        return null;
+    }
+
+    public static void printConstructorData(Class<?> clazz) {
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+
+        System.out.println(
+                String.format("class %s has %d declared constructors", clazz.getSimpleName(), constructors.length));
+
+        for (int i = 0; i < constructors.length; i++) {
+            Class<?>[] parameterTypes = constructors[i].getParameterTypes();
+
+            List<String> parameterTypeNames = Arrays.stream(parameterTypes)
+                    .map(Class::getSimpleName)
+                    .collect(Collectors.toList());
+
+            System.out.println(parameterTypeNames);
+        }
+    }
+
+    public static class Person {
+        private final Address address;
+        private final String name;
+        private final int age;
+
+        public Person() {
+            this(null, "anonymous", 0);
+        }
+
+        public Person(String name) {
+            this(null, name, 0);
+        }
+
+        public Person(String name, int age) {
+            this(null, name, age);
+        }
+
+        public Person(Address address, String name, int age) {
+            this.address = address;
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "address=" + address +
+                    ", name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+
+    private static class Address {
+        private final String street;
+        private final int number;
+
+        public Address(String street, int number) {
+            this.street = street;
+            this.number = number;
+        }
+
+        @Override
+        public String toString() {
+            return "Address{" +
+                    "street='" + street + '\'' +
+                    ", number=" + number +
+                    '}';
+        }
+    }
+}
